fb6ceef0ed152576f9294e97c76f2f983c995e8e
tat <tat>
2006-04-20 12:50:13 +0000

    [libu] set NO_{NET,ENV,HMAP,CONFIG,LOG} also in libu_conf.h to avoid .h inclusion

--- u/libu.h
+++ u/libu.h
@@ -8,18 +8,29 @@
 #include "libu_conf.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <u/config.h>
 #include <u/carpal.h>
-#include <u/env.h>
-#include <u/hmap.h>
 #include <u/log.h>
 #include <u/logprv.h>
 #include <u/memory.h>
 #include <u/misc.h>
-#include <u/net.h>
 #include <u/queue.h>
 #include <u/str.h>
 #include <u/uri.h>
 #include <u/os.h>
+#ifndef NO_NET
+#include <u/net.h>
+#endif
+#ifndef NO_ENV
+#include <u/env.h>
+#endif
+#ifndef NO_HMAP
+#include <u/hmap.h>
+#endif
+#ifndef NO_CONFIG
+#include <u/config.h>
+#endif
+#ifndef NO_LOG
+#include <u/log.h>
+#endif
 
 #endif /* !_U_LIBU_H_ */
