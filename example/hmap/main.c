c276f0bd0f2db63d13ffd86e9e310a35e0d508c8
stewy <stewy>
2006-12-22 14:23:54 +0000

    [libu] example/hmap: added static example; subdir Makefiles

--- example/hmap/main.c
+++ example/hmap/main.c
@@ -1,14 +1,77 @@
-/* $Id: main.c,v 1.1 2006/12/20 21:56:04 stewy Exp $ */
+/* $Id: main.c,v 1.2 2006/12/22 14:23:54 stewy Exp $ */
 
 #include <u/libu.h>
 
 int facility = LOG_LOCAL0;
 
+static int example_dynamic(void);
+static int example_static(void);
+
 int main()
+{
+    con_err_if (example_static());
+    con_err_if (example_dynamic());
+
+    return 0;
+err:    
+    return ~0;
+}
+
+static int example_static()
+{
+    u_hmap_opts_t *opts = NULL;
+    u_hmap_t *hmap = NULL;
+    int fibonacci[] = { 0, 1, 1, 2, 3, 5, 8, 13, 21 },
+        *x;
+
+    con("example_static()");
+
+    /* initialise options and hmap */
+    con_err_if (u_hmap_opts_new(&opts));
+    opts->f_free = NULL;                    /* static allocation */
+
+    con_err_if (u_hmap_new(opts, &hmap));
+
+    /* insert some sample elements */
+    con_err_if (u_hmap_put(hmap, "first", (void *) &fibonacci[0]));
+    con_err_if (u_hmap_put(hmap, "fifth", (void *) &fibonacci[4])); 
+    con_err_if (u_hmap_put(hmap, "last", (void *) 
+                &fibonacci[(sizeof(fibonacci)/sizeof(int))-1]));
+
+    /* retrieve and print values to console */
+    con_err_if (u_hmap_get(hmap, "last", (void **) &x)); 
+    con("hmap['last'] = %d", *x);
+    con_err_if (u_hmap_get(hmap, "fifth", (void **) &x)); 
+    con("hmap['fifth'] = %d", *x); 
+    con_err_if (u_hmap_get(hmap, "first", (void **) &x)); 
+    con("hmap['first'] = %d", *x);
+
+    /* remove an element and replace it - will use default free() function */
+    con_err_if (u_hmap_del(hmap, "fifth")); 
+
+    /* check that it has been deleted */
+    con_err_if (u_hmap_get(hmap, "fifth", (void **) &x) == 0); 
+
+    /* replace with a new element and print it */
+    con_err_if (u_hmap_put(hmap, "first", (void **) &x));
+    con_err_if (u_hmap_get(hmap, "last", (void **) &x)); 
+    con("hmap['last'] = %d", *x);
+
+    /* free hmap (options are freed automatically) */
+    u_hmap_free(hmap);
+    
+    return 0;
+err:
+    return ~0;
+}
+
+static int example_dynamic()
 {
     u_hmap_opts_t *opts = NULL;
     u_hmap_t *hmap = NULL;
     char *s = NULL;
+
+    con("example_dynamic()");
 
     /* initialise options and hmap */
     con_err_if (u_hmap_opts_new(&opts));
@@ -21,11 +84,11 @@ int main()
 
     /* retrieve and print values to console */
     con_err_if (u_hmap_get(hmap, "italian", (void **) &s)); 
-    con(s);
+    con("hmap['italian'] = %s", s);
     con_err_if (u_hmap_get(hmap, "german", (void **) &s)); 
-    con(s); 
+    con("hmap['german'] = %s", s); 
     con_err_if (u_hmap_get(hmap, "english", (void **) &s)); 
-    con(s);
+    con("hmap['english'] = %s", s);
 
     /* remove an element and replace it - will use default free() function */
     con_err_if (u_hmap_del(hmap, "german")); 
@@ -36,13 +99,12 @@ int main()
     /* replace with a new element and print it */
     con_err_if (u_hmap_put(hmap, "german", (void *) strdup("Auf Wiedersehen!")));
     con_err_if (u_hmap_get(hmap, "german", (void **) &s)); 
-    con(s);
+    con("hmap['german'] = %s", s);
 
     /* free hmap (options are freed automatically) */
     u_hmap_free(hmap);
     
     return 0;
-
 err:
     return ~0;
 }
