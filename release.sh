cfdb1b74d32b8668f8115e6e5485ca9c44ad13a8
tho <tho>
2008-04-05 15:19:47 +0000

    [libu] release.sh ++

--- release.sh
+++ release.sh
@@ -19,26 +19,50 @@ REL_OUT_DIR="${REL_DIR}/out"
 
 REL_TARGET="root@gonzo.koanlogic.com:/var/www-anemic/www/download/libu/"
 
+
+yesno ()
+{
+    /bin/echo -n "$1 " 
+    
+    while [ true ] 
+    do 
+        read answer
+        case ${answer} in
+            [Yy]) return 0 ;;
+            [nN]) return 1 ;;
+            *) /bin/echo -n "please say [yY] or [nN]: " ;; 
+        esac
+    done 
+}       
+
+err ()
+{
+    /bin/echo $@
+    exit 1
+}
+
 make_pre() 
 {
-    cd ${REL_WD}
     rm -r ${REL_TMP_DIR}
-    mkdir -p ${REL_TMP_DIR}
-    mkdir -p ${REL_OUT_DIR}
+    mkdir -p ${REL_TMP_DIR} || err "can't create directory ${REL_TMP_DIR}"
+    mkdir -p ${REL_OUT_DIR} || err "can't create directory ${REL_OUT_DIR}"
 }
 
 make_tag()
 {
+    yesno "using tag \"${REL_TAG}\", do you want to continue ?" \
+        || err "bailing out on client request"
+
     echo "==> tagging as \"${REL_TAG}\""
-    cvs tag -F ${REL_TAG}
+    cvs tag -F ${REL_TAG} || err "cvs tag command failed"
 }
 
 make_export()
 {
     echo "==> exporting tagged tree"
     pushd .
-    cd ${REL_TMP_DIR}
-    cvs export -r ${REL_TAG} ${REL_ROOT}
+    cd ${REL_TMP_DIR} || err "can't cd to ${REL_TMP_DIR}"
+    cvs export -r ${REL_TAG} ${REL_ROOT} || err "cvs export command failed"
     popd
 }
 
@@ -47,17 +71,23 @@ make_dist()
     echo "==> creating dist"
 
     pushd .
-    cd ${REL_TMP_DIR}/${REL_ROOT}
-    makl-conf
-    makl -f Makefile.dist dist
-    cp ChangeLog libu-${REL_VERSION}.* ${REL_OUT_DIR}
+    cd ${REL_TMP_DIR}/${REL_ROOT} \
+        || err "can't cd to ${REL_TMP_DIR}/${REL_ROOT}"
+
+    # we need doxygen path
+    makl-conf || err "makl-conf failed"
+    makl -f Makefile.dist dist || "dist target failed"
+
+    cp ChangeLog libu-${REL_VERSION}.* ${REL_OUT_DIR} \
+        || err "can't copy dist files to ${REL_OUT_DIR}"
     popd
 }
 
 make_upload()
 {
     echo "uploading release"
-    scp -r ${REL_OUT_DIR}/libu-${REL_VERSION}.* ChangeLog ${REL_TARGET}
+    scp -r ${REL_OUT_DIR}/libu-${REL_VERSION}.* ChangeLog ${REL_TARGET} \
+        || err "can't copy dist files to ${REL_TARGET}"
 }
 
 make_clean()
