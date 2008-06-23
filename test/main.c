7fa4425cd3f35e414231cc29595859baf1c65008
tho <tho>
2008-06-23 19:27:47 +0000

    [libu] string module test

--- test/main.c
+++ test/main.c
@@ -8,6 +8,7 @@ int main(int argc, char **argv)
     U_TEST_MODULE_USE(hmap);
     U_TEST_MODULE_USE(list);
     U_TEST_MODULE_USE(array);
+    U_TEST_MODULE_USE(string);
 
     return u_test_run(argc, argv);
 }
