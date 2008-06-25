6301cde624d73a6322c25ca9204867aa70981a32
tho <tho>
2008-06-25 09:29:23 +0000

    [libu] test: do test for enabled features only

--- test/main.c
+++ test/main.c
@@ -5,10 +5,16 @@ int facility = LOG_LOCAL0;
 int main(int argc, char **argv)
 {
     U_TEST_MODULE_USE(misc);
-    U_TEST_MODULE_USE(hmap);
-    U_TEST_MODULE_USE(list);
-    U_TEST_MODULE_USE(array);
     U_TEST_MODULE_USE(string);
+#ifndef NO_HMAP
+    U_TEST_MODULE_USE(hmap);
+#endif
+#ifndef NO_LIST
+    U_TEST_MODULE_USE(list);
+#endif
+#ifndef NO_ARRAY
+    U_TEST_MODULE_USE(array);
+#endif
 
     return u_test_run(argc, argv);
 }
