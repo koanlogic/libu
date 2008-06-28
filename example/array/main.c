4f17f85a9c4d59bae2f4667c67b6370a08d2730d
tho <tho>
2008-06-28 14:23:12 +0000

    [libu] array example wip

--- example/array/main.c
+++ example/array/main.c
@@ -6,10 +6,16 @@ int main (void)
 {
     u_array_t *a = NULL;
 
-    con_err_if (u_array_create(10, &a));
-    con_err_if (u_array_set_n(a, 12, (void *) 1234, NULL));
-    con_err_if (u_array_set_n(a, 20, (void *) 1234, NULL));
-    con_err_if (u_array_set_n(a, 100, (void *) 1234, NULL));
+    con_err_if (u_array_create(5, &a));
+
+    /* insert at high locations to force auto resize */
+    con_err_if (u_array_set_n(a, 7, (void *) 7, NULL));
+    con_err_if (u_array_set_n(a, 10, (void *) 10, NULL));
+    con_err_if (u_array_set_n(a, 30, (void *) 30, NULL));
+
+    /* dump data */
+    u_array_print(a);
+
     u_array_free(a);
 
     return 0;
