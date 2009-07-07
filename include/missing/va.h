3fa2144347317cef6865d0313e5fe9981e31cff4
tat <tat>
2009-07-07 08:44:10 +0000

    va.h added

---
+++ include/missing/va.h
@@ -0,0 +1,18 @@
+/* 
+ * Copyright (c) 2005-2009 by KoanLogic s.r.l. - All rights reserved.  
+ */
+#ifndef _LIBU_VA_H_
+#define _LIBU_VA_H_
+#include <u/libu_conf.h>
+
+#if defined(va_copy)
+   /* C99 va_copy */
+   #define u_va_copy(a, b)  va_copy(a, b)
+#elif defined(__va_copy)
+   /* GNU libc va_copy replacement */
+   #define u_va_copy(a, b)  __va_copy(a, b)
+#else
+  #error "va_copy is not defined"
+#endif
+
+#endif
