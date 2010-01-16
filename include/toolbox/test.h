2307749eb6ce947ec1d0777f44696cf75ebd1872
tho <tho>
2010-01-16 09:44:48 +0000

    [libu] test module review (minor fixes + doxy)

--- include/toolbox/test.h
+++ include/toolbox/test.h
@@ -3,138 +3,62 @@
 #include <u/libu.h>
 
 /**
- *  \defgroup test Unit testing
+ *  \addtogroup test
  *  \{
- *
- *      Tests are bundles in test modules defined by the U_TEST_MODULE macro; 
- *      each module contain one or more test functions; such functions will 
- *      be considered succesfull when returning 0. Function should be run
- *      from within the module main function wrapped by the U_TEST_RUN macro.
- *      
- *
- *      Example: mymodule.c
- *      \code
- * 
- *          int test_feature_1(void)
- *          {
- *              dbg_err_if(...)
- *
- *              return 0; 
- *          err:
- *              return ~0;
- *          }
- *
- *          int test_feature_2(void)
- *          {
- *              dbg_err_if(
- *
- *              return 0; 
- *          err:
- *              return ~0;
- *          }
- *
- *          U_TEST_MODULE( mymodule )
- *          {
- *              U_TEST_RUN( test_feature_1 );
- *              U_TEST_RUN( test_feature_2 );
- *          
- *              return 0;                                                
- *          }
- *      \endcode
- * 
- *
- *      To build the 'runtest' executable define a main() function like 
- *      the example below and import your test modules using the 
- *      U_TEST_USE_MODULE macro. 
- *
- *      Example: main.c
- *      \code
- *          #include <u/libu.h>
- *
- *          int facility = LOG_LOCAL0;
- *
- *          int main(int argc, char **argv)
- *          {
- *              U_TEST_USE_MODULE( mymodule );
- *              U_TEST_USE_MODULE( mymodule2 );
- *          
- *              return u_test_run(argc, argv);
- *          }
- *      \endcode
- *
- *
- *      To run all tests call the built executable with no arguments. At the
- *      end of execution it will print the number of tests run and the number
- *      ot tests failed.
- *          \verbatim
- *          ./runtest # runs all modules' test
- *          \endverbatim
- *
- *      If you want to run just selected modules add their name to the 'runtest'
- *      command line:
- *          \verbatim
- *          ./runtest mymodule # runs just 'mymodule' tests
- *          ./runtest mymodule othermod # runs 'mymodule' and 'othermod' tests
- *          \endverbatim
- */
+ */ 
 
-/** \brief Define a test module
+/** 
+ *  \brief Define a test module
  *
- * Defines a test module named \c name. 
+ *  Defines a test module named \c name. 
  *
- * \param name  module name
+ *  \param name  module name
  */
-#define U_TEST_MODULE( name ) \
+#define U_TEST_MODULE( name )   \
     int u_test_run_ ## name (void)
 
-/** \brief Define a test function
+/** 
+ *  \brief Define a test function
  *
- * Defines a test function.
+ *  Defines a test function.
  *
- * \param f     the function name
- *
- * \return \c 0 on success, not zero on failure (i.e. test failed)
+ *  \param  f   the function name
  */
 #define U_TEST_RUN( f ) \
-    if( f () ) { _test_cnt++; _test_fail++; u_con("%s: failed", #f); } \
+    if( f () ) { _test_cnt++; _test_fail++; u_con("%s: failed", #f); }  \
     else { _test_cnt++; _test_ok++; if(_verbose) u_con("%s: ok", #f); }
 
-
-/** \brief Import a test module in the test program
+/** 
+ *  \brief Import a test module in the test program
  *
- * Import a test module to be use by the test program. 
+ *  Import a test module to be use by the test program. 
  *
- * \param name     the name of the module
- *
+ *  \param  name    the name of the module
  */
-#define U_TEST_MODULE_USE( name ) \
-    do {    \
-        int u_test_run_ ## name (void); \
-        *_top = u_test_run_ ## name; ++_top; *_top = NULL; \
-        *_top_nm = u_strdup( #name ); ++_top_nm; *_top_nm = NULL; \
+#define U_TEST_MODULE_USE( name )                                   \
+    do {                                                            \
+        int u_test_run_ ## name (void);                             \
+        *_top = u_test_run_ ## name; ++_top; *_top = NULL;          \
+        *_top_nm = u_strdup( #name ); ++_top_nm; *_top_nm = NULL;   \
     } while(0)
 
-/** \brief Run tests
- *
- *  Run tests. Must be called by the main() function. Returns when all tests
- *  have been run.
- *
- * \param argc  main() argc argument
- * \param argv  main() argv argument
- *
- * \return \c 0 on success, not zero on failure
- */
 int u_test_run(int argc, char **argv);
 
-/** */
+/** \brief  The prototype for a test function.  The function must return
+ *          \c 0 on success, \c ~0 on error. */
 typedef int (*test_runner_t)(void);
-/** */
+
+/** \brief  The array which holds all the test functions (INTERNAL) */
 extern test_runner_t _mods[], *_top;
-/** */
+
+/** \brief  The array which holds all modules names (INTERNAL) */
 extern char *_mods_nm[], **_top_nm;
-/** */
+
+/** \brief  Various internal counters */
 extern int _test_cnt, _test_ok, _test_fail;
-/** */
+
+/** \brief  If set, via <code>runtest -v</code>, more info about the running
+ *          tests are printed to standard error */
 extern int _verbose;
 
 /**
--- srcs/toolbox/test.c
+++ srcs/toolbox/test.c
@@ -20,6 +20,128 @@ int _verbose = 0;
 static char **arg;
 static int narg;
 
+static int parse_opt(int argc, char **argv);
+static void usage(const char *prog);
+static int run_test_module(const char *module);
+
+/**
+    \defgroup test Unit testing
+    \{
+        Tests are bundles in test modules defined by the ::U_TEST_MODULE macro; 
+        each module contain one or more test functions; such functions will 
+        be considered succesfull when returning \c 0. Function should be run
+        from within the module main function wrapped by the ::U_TEST_RUN macro.
+  
+        Example: mymodule.c
+    \code
+    int test_feature_1(void)
+    {
+        dbg_err_if(...);
+
+        return 0; 
+    err:
+        return ~0;
+    }
+
+    int test_feature_2(void)
+    {
+        dbg_err_if(...);
+
+        return 0; 
+    err:
+        return ~0;
+    }
+
+    U_TEST_MODULE( mymodule )
+    {
+        U_TEST_RUN( test_feature_1 );
+        U_TEST_RUN( test_feature_2 );
+    
+        return 0;                                                
+    }
+    \endcode
+
+        To build a \c runtest executable define a \c main() function like 
+        the example below and import your test modules using the 
+        ::U_TEST_USE_MODULE macro. 
+  
+        Example: main.c
+    \code
+    #include <u/libu.h>
+
+    int facility = LOG_LOCAL0;
+
+    int main(int argc, char **argv)
+    {
+        U_TEST_USE_MODULE( mymodule );
+        U_TEST_USE_MODULE( mymodule2 );
+    
+        return u_test_run(argc, argv);
+    }
+    \endcode
+
+        To run all tests call \c runtest with no arguments. At the end of 
+        execution it will print the number of tests run and the number of 
+        tests that have failed.  Add the \c -v switch if you want a more
+        verbose output.
+    \code
+    $ ./runtest [-v] # run testers of all the defined U_TEST_USE_MODULE's 
+    \endcode
+
+        If you want to run just selected modules add their name to the 
+        \c runtest command line:
+    \code
+    $ ./runtest mymodule # just run 'mymodule' tests
+    $ ./runtest mymodule othermod # run 'mymodule' and 'othermod' tests
+    \endcode
+ */
+
+/** 
+ *  \brief Run tests
+ *
+ *  Run tests. It shall be called by the \c main() function and will return 
+ *  when all tests have been run.
+ *
+ *  \param  argc    \c main() \c argc argument
+ *  \param  argv    \c main() \c argv argument
+ *
+ *  \retval  0  on success
+ *  \retval ~0  on failure
+ */
+int u_test_run(int argc, char **argv)
+{
+    test_runner_t *p;
+    char **pn;
+    int i;
+
+    dbg_err_if(parse_opt(argc, argv));
+
+    if(narg)
+    {
+        /* run only user provided modules */
+        for(i = 0; i < narg; ++i)
+            dbg_err_if(run_test_module(arg[i]));
+    } else {
+        /* run all modules */
+        for(p = _mods; p < _top; ++p)
+            (*p)();
+    }
+
+    /* free strdup'd module names */
+    for(pn = _mods_nm; pn != _top_nm; ++pn)
+        u_free(*pn), *pn = NULL;
+
+    u_con("%d test run, %d failed", _test_cnt, _test_fail);
+
+    return 0;
+err:
+    return ~0;
+}
+
+/**
+ *  \}
+ */
+
 static void usage(const char *prog)
 {
     static const char *us = 
@@ -31,11 +153,10 @@ static void usage(const char *prog)
         "    Available modules:\n";
     char **p;
 
-    fprintf(stderr, us, prog);
+    u_con(us, prog);
 
     for(p = _mods_nm; p != _top_nm; ++p)
-        fprintf(stderr, "        %s\n", *p);
-    fprintf(stderr, "\n");
+        u_con("        %s", *p);
 
     exit(1);
 }
@@ -79,34 +200,4 @@ static int run_test_module(const char *module)
 
     con_return_ifm (1, ~0, "unknown module %s", module);
 }
-
-int u_test_run(int argc, char **argv)
-{
-    test_runner_t *p;
-    int i;
-
-    dbg_err_if(parse_opt(argc, argv));
-
-    if(narg)
-    {
-        /* run only user provided modules */
-        for(i = 0; i < narg; ++i)
-            dbg_err_if(run_test_module(arg[i]));
-    } else {
-        /* run all modules */
-        for(i = 0, p = _mods; p < _top; ++p, ++i)
-            (*p)();
-    }
-
-    /* free strdup'd module names */
-    for(p = _mods_nm; p != _top_nm; ++p)
-        u_free(*p), *p = NULL;
-
-    printf("%d test run, %d failed\n", _test_cnt, _test_fail);
-
-    return 0;
-err:
-    return ~0;
-}
-
 
