a9b8529ef22d7824b6e964bfd0360ca3b6083e74
tho <tho>
2010-01-24 22:20:58 +0000

    [libu] sample uri: align print interface

--- example/uri/main.c
+++ example/uri/main.c
@@ -8,8 +8,8 @@ int main (int ac, char *av[])
 
     con_err_ifm (ac != 2, "uuri <uri string>");
 
-    con_err_if (u_uri_crumble(av[1], 0, &u));
-    u_uri_print(u);
+    con_err_ifm (u_uri_crumble(av[1], 0, &u), "URI parse error");
+    u_uri_print(u, 0);
     u_uri_free(u);
 
     return 0;
