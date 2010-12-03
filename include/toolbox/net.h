2520e4cd1a430b50884764dedead4f28905785c3
tho <tho@koanlogic.com>
2010-12-03 21:25:28 +0100

    mingw ETIMEDOUT/EINPROGRESS tentative fix

--- include/toolbox/net.h
+++ include/toolbox/net.h
@@ -41,6 +41,8 @@
   #include <winsock2.h>
   #include <ws2tcpip.h>
 
+  #define EINPROGRESS   WSAEWOULDBLOCK
+  #define ETIMEDOUT     WSAETIMEDOUT
   #define EAFNOSUPPORT  WSAEAFNOSUPPORT
 #endif  /* OS_WIN */
 
